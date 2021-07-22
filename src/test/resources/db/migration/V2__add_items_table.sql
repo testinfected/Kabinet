e0ba55f2483092cbb24e7a10cc4fa9342c15e870
Vincent Tencé <vtence@gmail.com>
2021-07-22 18:24:54 -0400

    Remove unnecessary files

--- src/test/resources/db/migration/V2__add_items_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE items
-(
-    id         BIGINT         NOT NULL AUTO_INCREMENT,
-    number     VARCHAR(8)     NOT NULL UNIQUE,
-    product_id BIGINT         NOT NULL,
-    price      DECIMAL(10, 2) NOT NULL,
-    PRIMARY KEY (id),
-    CONSTRAINT item_product FOREIGN KEY (product_id) REFERENCES products (id)
-);
